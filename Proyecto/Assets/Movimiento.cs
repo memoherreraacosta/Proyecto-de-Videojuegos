9d4e7a011c9762b0e70a11663364677c2f32409d
Guillermo Herrera <memoherreraacosta@gmail.com>
2018-08-28 10:58:29 -0500

    Se agrego la carpeta del proyecto en el que esta la base del videojuego

---
+++ Proyecto/Assets/Movimiento.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+    public Rigidbody RB;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0,0,Input.GetAxis("Vertical")), Space.Self);
+        transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal"),0));
+    }
+}
---
+++ Proyecto/Assets/Pruebas/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public Rigidbody RB;
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Translate(new Vector3(0,0,Input.GetAxis("Vertical")), Space.Self);
+        //transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal"),0));
+        RB.AddForce(Vector3.forward * Input.GetAxis("Vertical") * 70000);
+        RB.AddForce(Vector3.left * -Input.GetAxis("Horizontal") * 35000);
+    }
+}
